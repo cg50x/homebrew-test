9a4a75d0a67fafc21258193ad08e31b3d37cc475
Chris Guevara <chrismguevara@gmail.com>
2018-02-06 15:19:23 -0800

    Fix zip file

--- Formula/triton-kubernetes.rb
+++ Formula/triton-kubernetes.rb
@@ -3,7 +3,7 @@ class TritonKubernetes < Formula
     homepage "https://github.com/nimajalali/triton-kubernetes"
     url "https://s3-us-west-2.amazonaws.com/cguevara-learn-brew/0.0.1/mac_osx/triton-kubernetes.zip"
     version "0.0.1"
-    sha256 "4ca2e0f9507cba4df38bd8b7c6c663ddd4faa9d0ae6bae86837561edc8ead55e"
+    sha256 "a3d0d87125db6ed03297733fd7ac865b7815bda06433d2076abb3de6ad616d21"
 
     def install
         bin.install "triton-kubernetes"
