4c2d5e03b7e944e2fcfb4128ec9fa6b05c5d670e
Chris Guevara <chrismguevara@gmail.com>
2018-02-06 16:04:55 -0800

    Use github releases for url

--- Formula/triton-kubernetes.rb
+++ Formula/triton-kubernetes.rb
@@ -1,7 +1,7 @@
 class TritonKubernetes < Formula
     desc "triton-kubernetes command line tool"
     homepage "https://github.com/nimajalali/triton-kubernetes"
-    url "https://s3-us-west-2.amazonaws.com/cguevara-learn-brew/0.0.1/mac_osx/triton-kubernetes.zip"
+    url "https://github.com/cg50x/homebrew-test/releases/download/0.0.1/triton-kubernetes.zip"
     version "0.0.1"
     sha256 "a3d0d87125db6ed03297733fd7ac865b7815bda06433d2076abb3de6ad616d21"
 
