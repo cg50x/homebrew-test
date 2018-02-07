46852d5d40b12f057a158978860c015fa7471526
Chris Guevara <chrismguevara@gmail.com>
2018-02-07 12:20:33 -0800

    Set url to test-release tag and build from source

--- Formula/triton-kubernetes.rb
+++ Formula/triton-kubernetes.rb
@@ -1,11 +1,20 @@
 class TritonKubernetes < Formula
     desc "triton-kubernetes command line tool"
     homepage "https://github.com/nimajalali/triton-kubernetes"
-    url "https://github.com/cg50x/homebrew-test/releases/download/0.0.1/triton-kubernetes.zip"
+    url "https://github.com/nimajalali/triton-kubernetes/archive/test-release.tar.gz"
     version "0.0.1"
-    sha256 "a3d0d87125db6ed03297733fd7ac865b7815bda06433d2076abb3de6ad616d21"
+    sha256 "50be2d15864bb944f0c5ce514deff788bd53f7cf39c414cdc39220ffbde4ea7b"
+    head "https://github.com/nimajalali/triton-kubernetes.git"
+
+    depends_on "go" => :build
 
     def install
+        ENV["GOPATH"] = buildpath
+        dir = buildpath/"src/github.com/joyent/triton-kubernetes"
+        dir.install buildpath.children
+        cd "src/github.com/joyent/triton-kubernetes" do
+            system "go", "build"
+        end
         bin.install "triton-kubernetes"
     end
 end
